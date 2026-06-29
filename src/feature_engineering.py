71bc32ea48f2400b043e99061144bfe5187d6e90
citanakberfin97-code <citanakberfin97@gmail.com>
2026-06-29 17:30:38 +0300

    Update feature_engineering.py

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -9,4 +9,11 @@ FROM saas
 GROUP BY Company
 """).df()
 
-summary
+summary 
+
+Company	avg_revenue	avg_growth	avg_magic_number	avg_net_income
+0	CRM	1.053160e+10	0.032351	NaN	1.912800e+09
+1	ADBE	6.214200e+09	0.030326	inf	1.784000e+09
+2	MSFT	7.766780e+10	0.043324	inf	3.020800e+10
+3	SHOP	2.945200e+09	0.087804	NaN	1.300000e+08
+4	HUBS	8.024524e+08	0.053953	inf	1.569300e+07
